0cfeb44ff4acd58b50bda790a02fd0ccb0294c6a
GitHub Action <action@github.com>
2026-01-12 12:49:56 +0000

    Update job data via GitHub Action [skip ci]

--- ui/jobs.js
+++ ui/jobs.js
@@ -31,7 +31,7 @@ window.JOBS_DATA = [
     "state": "CA",
     "title": "Sr. AI Platform Data Engineer, Ring Decision Science",
     "university_job": null,
-    "updated_time": "6 days",
+    "updated_time": "7 days",
     "url_next_step": "https://account.amazon.com/jobs/3152458/apply",
     "team.id": null,
     "team.business_category_id": null,
@@ -305,7 +305,7 @@ window.JOBS_DATA = [
     "title": "Staff Data Platform Engineer",
     "company": "CVS Health",
     "location": "NC - Work from home",
-    "posted_date": "2026-01-08T06:38:18.288471",
+    "posted_date": "2026-01-07T12:49:54.591929",
     "url_next_step": "https://cvshealth.wd1.myworkdayjobs.com/en-US/CVS_Health_Careers/job/NC---Work-from-home/Staff-Data-Platform-Engineer_R0756986",
     "description_short": "Staff Data Platform Engineer",
     "source": "CVS Health"
@@ -335,7 +335,7 @@ window.JOBS_DATA = [
     "title": "Associate Cloud Data Engineer",
     "company": "CVS Health",
     "location": "TX - Irving",
-    "posted_date": "2026-01-09T06:38:17.967276",
+    "posted_date": "2026-01-08T12:49:54.241789",
     "url_next_step": "https://cvshealth.wd1.myworkdayjobs.com/en-US/CVS_Health_Careers/job/TX---Irving/Associate-Cloud-Data-Engineer_R0781617",
     "description_short": "Associate Cloud Data Engineer",
     "source": "CVS Health"
@@ -405,7 +405,7 @@ window.JOBS_DATA = [
     "title": "Analytic Data Engineer",
     "company": "CVS Health",
     "location": "50 Locations",
-    "posted_date": "2026-01-10T06:38:17.967208",
+    "posted_date": "2026-01-09T12:49:54.241703",
     "url_next_step": "https://cvshealth.wd1.myworkdayjobs.com/en-US/CVS_Health_Careers/job/Treehouse/Analytic-Data-Engineer_R0790701-1",
     "description_short": "Analytic Data Engineer",
     "source": "CVS Health"
